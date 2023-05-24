3fc43738868263743bf83437f53717008adf629b
Lucas Santos <lucassantosdev@hotmail.com>
2023-05-24 19:06:25 -0300

    24.05_19:00

---
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-painel-esquerdo',
+  templateUrl: './painel-esquerdo.component.html',
+  styleUrls: ['./painel-esquerdo.component.scss']
+})
+export class PainelEsquerdoComponent implements OnInit {
+
+  menuSelecionado = 'Home';
+
+
+//icones
+
+  homeIcone = faHome;
+  pesquisarIcone = faSearch;
+  artistaIcone = faGuitar;
+  playlist = faMusic;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  botaoClick(botao: string){
+    this.menuSelecionado = botao;
+  }
+
+}
